a609f6dc9a25322b68a11575d5ce3430787d1b95
IsYet1 <Don@IsYet.com>
2021-11-24 15:46:53 -0600

    Initial Commit

---
+++ U_FungiFinder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  U_FungiFinder
+//
+//  Created by Don McKenzie on 11/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ U_FungiFinder/U_FungiFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  U_FungiFinderApp.swift
+//  U_FungiFinder
+//
+//  Created by Don McKenzie on 11/24/21.
+//
+
+import SwiftUI
+
+@main
+struct U_FungiFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
