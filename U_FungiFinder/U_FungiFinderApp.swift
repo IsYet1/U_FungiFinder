d10edef86e720f22f36b9d2b2fefa765ab8e0192
IsYet1 <Don@IsYet.com>
2021-11-24 17:07:59 -0600

    Added packages. Added test task save. Verified it's working.

--- U_FungiFinder/U_FungiFinderApp.swift
+++ U_FungiFinder/U_FungiFinderApp.swift
@@ -6,9 +6,13 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct U_FungiFinderApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
